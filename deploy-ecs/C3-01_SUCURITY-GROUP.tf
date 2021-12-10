4056fd35e6e4e71ee2e330fa0456bc889c6bba09
Hernan Aguero <hernaaguero@gmail.com>
2021-12-09 23:32:37 -0300

    Changing open ports in seurity group

--- deploy-ecs/C3-01_SUCURITY-GROUP.tf
+++ deploy-ecs/C3-01_SUCURITY-GROUP.tf
@@ -1,13 +1,8 @@
 resource "aws_security_group" "ecs_sg" {
   name        = "ecs_sg"
-  description = "Allow SSH and HTTP traffic"
+  description = "Allow HTTP traffic"
   vpc_id = "vpc-76df431d"
-  ingress {
-        from_port     = 22
-        to_port       = 22
-        protocol      = "tcp"
-        cidr_blocks   = ["0.0.0.0/0"]
-  }
+
   ingress {
         from_port     = 80
         to_port       = 80
