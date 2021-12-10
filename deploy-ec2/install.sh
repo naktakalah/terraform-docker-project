ecd631814e5a94c4055a168255b9f70a6587035f
naktakalah <60784236+naktakalah@users.noreply.github.com>
2021-12-09 22:55:06 -0300

    Delete deploy-ec2 directory

--- deploy-ec2/install.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-sudo yum update -y
-sudo amazon-linux-extras install docker -y
-sudo systemctl start docker.service
-sudo usermod -a -G docker ec2-user
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo docker login --username docktor1994 --password K4BwyA3MXXK6
-sudo docker run -d --rm -p 80:80 docktor1994/devops_practices:test_node
